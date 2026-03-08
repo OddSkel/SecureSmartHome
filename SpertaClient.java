f7011feb60b7f13142254c5b93922071fbe5ea8f
Manuel Leitão <manuel.t.leitao@gmail.com>
2026-03-08 13:52:03 +0000

    Auth and Client initialization done

--- SpertaClient.java
+++ SpertaClient.java
@@ -7,25 +7,60 @@ import java.util.Scanner;
 
 public class SpertaClient {
   private int port;
+  private String host;
+  private String user, pwd;
+
+  private static final String COMMAND_LIST =
+  "Available Commands:\n" +
+  "CREATE <hm>\n" +
+  "ADD <user1> <hm> <a>\n" +
+  "RD <hm> <s>\n" +
+  "EC <hm> <d> <int>\n" +
+  "RT <hm>\n" +
+  "RH <hm> <d>";
   public static void main(String[] args) {
-    System.out.println("cliente: main");
+    if (args.length != 3) {
+      System.out.println("Usage: java SpertaClient <host:port> <username> <password>");
+      System.exit(-1);
+    }
+
+    String[] serverAddress = args[0].split(":");
 
     SpertaClient client = new SpertaClient();
-    client.port = Integer.parseInt(args[0]);
+    client.user = args[1];
+    client.pwd = args[2];
+    client.host = serverAddress[0];
+    switch (serverAddress.length) {
+      case 2 -> client.port = Integer.parseInt(serverAddress[1]);
+      case 1 -> client.port = 22345;
+      default -> {
+        System.out.println("Usage: server address should be in the format <host> or <host:port>");
+        System.exit(-1);
+      }
+    }
 
     client.startClient();
   }
 
   public void startClient(){
-    try(Socket cliSoc = new Socket("localhost", port);
-        ObjectInput serverInfo = new ObjectInputStream(cliSoc.getInputStream());
-        ObjectOutputStream clientInfo = new ObjectOutputStream(cliSoc.getOutputStream());
+    try(Socket cliSoc = new Socket(host, port);
+        ObjectInput inStream = new ObjectInputStream(cliSoc.getInputStream());
+        ObjectOutputStream outStream = new ObjectOutputStream(cliSoc.getOutputStream());
         Scanner user_input = new Scanner(System.in)) {
 
+      outStream.writeObject(user);
+      outStream.writeObject(pwd);
+      outStream.flush();
+      checkSResp(inStream, outStream, user_input);
+      
+      System.out.println(COMMAND_LIST);
 			while(true) {
-
+        System.out.print("Insert command: ");
+        String line = user_input.nextLine().trim();
+        if (line.isEmpty()) {
+            continue; 
+        }
 				String user_Command = user_input.next();
-
 				switch (user_Command) {
 					case "CREATE" -> {
                                 
@@ -46,9 +81,9 @@ public class SpertaClient {
                                 
           }
 					default -> {
-            clientInfo.writeObject(user_Command);
-            String server_Response = (String) serverInfo.readObject();
-            System.out.println(server_Response + "Commands Available: CREATE, ADD, RD, EC, RT, RH");
+            outStream.writeObject(user_Command);
+            String server_Response = (String) inStream.readObject();
+            System.out.println(server_Response + COMMAND_LIST);
           }
 				}
 			}
@@ -57,4 +92,23 @@ public class SpertaClient {
       System.exit(-1);
 		}
 	}
+
+  private void checkSResp(ObjectInput in, ObjectOutputStream out, Scanner sc) {
+    try{
+      boolean userOk = false;
+      while(!userOk){
+        String serverMsg = (String) in.readObject();
+        if(serverMsg.equals("WRONG_PWD")){
+          System.out.print("Inserir novamente palavra-passe:");
+          pwd = sc.nextLine();
+          out.writeObject(pwd);
+          out.flush();
+        } else {
+          userOk = true;
+        }
+      }
+    } catch (IOException | ClassNotFoundException e) {
+      System.err.println(e.getMessage());
+    }
+  }
 }
